34a96feb1ce6c35d4f50a429569cfe7bde31eb53
danpropc <165559472+danpropc@users.noreply.github.com>
2025-12-11 00:35:15 +0700

    Add files via upload

---
+++ File bài tập tin hoc.py
@@ -0,0 +1,24 @@
+#Bài 1:
+###a, b = (int(x) for x in input("Nhập 2 số a và b: ").split())
+###print("Tổng của a và b là:", a + b )
+
+#Bài 2:
+###a, b = (int(x) for x in input("Nhập chiều rộng và chiều dài: ").split())
+###print("Diện tích hình chữ nhật là:", a * b)
+
+#Bài 3:
+###a,b,c = (int(x) for x in input("Nhập 3 cạnh a, b và c: ").split())
+###print("Chu vi tam giác là:", a + b + c)
+    
+#Bài 4:
+###a,b,c,d = (int(x) for x in input("Nhập a, b, c, d:").split())
+###print("Trung bình cộng của a, b, c, d là:", round((a + b + c + d)/4,2))
+
+#Bài 5:
+###import math
+###a = int(input("Nhập bán kính hình tròn: "))
+###print("Diện tích hình tròn là:", round(math.pi * a * a,2))
+
+#Bài 6:
+###a, b = (int(x) for x in input("Nhập năm sinh và năm hiện tại: ").split())
+###print("Tuổi của bạn là:", b - a)
