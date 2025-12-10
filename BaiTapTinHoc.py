80965c33242f6c248ca0dfe83516c62fde63ce00
danpropc <165559472+danpropc@users.noreply.github.com>
2025-12-11 00:39:09 +0700

    Add files via upload

---
+++ BaiTapTinHoc.py
@@ -0,0 +1,24 @@
+#Bài 1 : Tính tổng 2 số a và b
+#a,b = (int(x) for x in input("Nhập 2 số a và b: ").split())
+#print("Tổng của a và b là: ", a + b)
+
+#Bài 2 : Tính diện tích Hình chữ nhật
+#a,b = (int(x) for x in input("Nhập chiều rộng và chiều dài: ").split())
+#print("Diện tích hình chữ nhật là:", a * b )
+
+#Bài 3 : Tính chu vi của tam giác
+#a,b,c = (int(x) for x in input("Nhập 3 số nguyên a, b và c: ").split())
+#print("Chu vi của tam giác:", a + b + c )
+
+#Bài 4 : Tìm giá trị trung bình cộng
+#a,b,c,d = (int(x) for x in input("Nhập 4 số tự nhiên a, b, c và d: ").split())
+#print("Gía trị trung bình cộng của a, b, c, d là:", round((a + b + c + d)/4,2))
+
+#Bài 5 : Tính diện tích hình tròn
+#import math
+#r = int(input("Nhập bán kính hình tròn:"))
+#print("Diện tích hình tròn là:", round(math.pi * r * r,2))
+
+#Bài 6 : Tính số tuổi hiện tại
+#a,b = (int(x) for x in input("Nhập năm sinh và năm hiện tại: ").split())
+#print("Tuổi hiện tại tìm được là:", b - a)
